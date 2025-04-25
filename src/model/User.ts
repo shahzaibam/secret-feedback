bad41e6b2306e490d685ca9c59f8a5f1525b7b78
Shah Zaib <shahzaibam05@gmail.com>
2025-04-25 18:32:01 +0200

    defined User Model

---
+++ src/model/User.ts
@@ -0,0 +1,69 @@
+import mongoose, { Schema, Document } from "mongoose";
+import { Message, MessageSchema } from "./Message";
+
+export interface User extends Document {
+  username: string;
+  email: string;
+  password: string;
+  verifyCode: string;
+  verifyCodeExpiry: Date;
+  isAcceptingMessage: boolean;
+  isVerified: boolean;
+  messages: Message[];
+}
+
+const UserSchema: Schema<User> = new Schema({
+  username: {
+    type: String,
+    required: [true, "Username is required"],
+    trim: true,
+    unique: true,
+  },
+
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    match: [
+      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+      "Please use a valid email address",
+    ],
+  },
+
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    minlength: [8, "Password must be at least 8 characters long"],
+  },
+
+  verifyCode: {
+    type: String,
+    required: [true, "Verify code is required"],
+  },
+
+  verifyCodeExpiry: {
+    type: Date,
+    required: [true, "Verify code expiry is required"],
+  },
+
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+
+  isAcceptingMessage: {
+    type: Boolean,
+    default: true,
+  },
+
+  messages: {
+    type: [MessageSchema],
+    default: [],
+  },
+});
+
+const UserModel =
+  (mongoose.models.User as mongoose.Model<User>) ||
+  mongoose.model<User>("User", UserSchema);
+
+export default UserModel;
